7efb1f527d2013574b162a106ba5b573988596e8
Daubar21 <daubar.carlos@gmail.com>
2025-12-12 11:48:08 +0100

    Update conexionBBDD.php

--- conexionBBDD.php
+++ conexionBBDD.php
@@ -1,10 +1,3 @@
 <?php
-
-$Repit=false;
-$host="localhost";
-$user="root";
-$password="";
-
-$link= mysqli_connect($host,$user,$password);
-$tildes=$link->query("SET NAMES 'utf8'");
-mysqli_select_db($link,'periodicos');
+$link = false; 
+?>
